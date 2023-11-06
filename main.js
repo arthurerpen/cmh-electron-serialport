c784306d9715b9be83c4e2969f85e5a596861dc7
arthurerpen <arthurerpen@gmail.com>
2023-11-06 20:13:17 -0300

    adicionado serverwebsocket para a balança

--- main.js
+++ main.js
@@ -19,12 +19,9 @@ function createWindow() {
         }
     })
 
-    // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+    mainWindow.loadURL("http://localhost:5173/entrada");
+
+    mainWindow.webContents.openDevTools();
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -60,3 +57,64 @@ app.on('activate', function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+
+const serialport = new SerialPort({ path: 'COM5', baudRate: 9600 })
+const parser = serialport.pipe(new ReadlineParser())
+
+var server = http.createServer(function(request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+});
+
+server.listen(8080, function() {
+  console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false
+});
+
+
+
+function originIsAllowed(origin) {
+// put logic here to detect whether the specified origin is allowed.
+return true;
+}
+
+wsServer.on('request', function(request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+  
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  
+  parser.on('data', (stream) => {connection.sendUTF(stream);console.log(stream)})
+
+  connection.on('close', function(reasonCode, description) {
+      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+});
+
+
+function lerPeso() {
+  serialport.write('P\r')
+  setTimeout(lerPeso, 2000);
+}
+
+lerPeso();
